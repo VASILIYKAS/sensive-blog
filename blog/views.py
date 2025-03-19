5fd22757da5eac592a48cb650723690ca8645420
VASILIYKAS <yoyoynv@gmail.com>
2025-03-19 13:43:08 +0500

    replace all sorted calls with order_by on main page.

--- blog/views.py
+++ blog/views.py
@@ -82,9 +82,9 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(tags_count=Count('posts'))
+    popular_tags = tags.order_by('-tags_count')
+    most_popular_tags = popular_tags[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -101,9 +101,9 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    tags = Tag.objects.annotate(tags_count=Count('posts'))
+    popular_tags = tags.order_by('-tags_count')
+    most_popular_tags = popular_tags[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
